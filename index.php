a611814b2d74db4f00a7d80afb825525107663b4
poulomi-03 <poulomibhattacharya03@gmail.com>
2024-05-23 08:12:01 +0530

    connection donei

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $password = "";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
-
+//establish connection
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
